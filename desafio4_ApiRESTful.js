d820d90310432a8c2fa47f47308ad9367d07da2e
FernandoMingrone <fernandomingrone@gmail.com>
2022-01-31 14:48:16 -0300

    Desafio 4 Api RESTful

--- desafio4_ApiRESTful.js
+++ desafio4_ApiRESTful.js
@@ -1,15 +1,27 @@
+/* Realizar un proyecto de servidor basado en node.js y express que ofrezca una API RESTful de productos. En detalle, que incorpore las siguientes rutas:
+
+GET '/api/productos' -> devuelve todos los productos.
+GET '/api/productos/:id' -> devuelve un producto según su id.
+POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+DELETE '/api/productos/:id' -> elimina un producto según su id. */
+
+
 const express = require("express");
-const { Router } = express;
-let multer = require("multer");
-// let path = require("path");
+const contenedor = require("./contenedor");
 const app = express();
+// let multer = require("multer");
+// let path = require("path");
 const PORT = 8080;
 let FRASE = `Desafío 4 Api RESTful`;
 
-let productos = [];
+const { Router } = express;
 
+let productos = [];
+let routerProductos = new Router();
 
 // app.use("/folder", express.static("Clase18135"));
+//busca el index.html
 app.use(express.static("Clase18135"));
 // http://localhost:8080/public/
 
@@ -19,124 +31,90 @@ app.use(express.urlencoded({extended:true}));
 
 
 
-let routerProductos = new Router();
 // let routerPersonas = new Router();
 
 
-//////1)
+
+//1) GET '/api/productos' -> devuelve todos los productos.
 
 routerProductos.get("/", (req, res, next)=>{ 
-    res.json(productos);
+    contenedor.getAll().then(data=> {
+        let productos = JSON.parse(data);
+        res.json(productos);
+    })
 
     //devuelve todos los productos
 })
 
-//////3)
-
-routerProductos.post("/", (req, res, next)=>{ 
-    productos.push(req.body)
-    res.json(productos);
-
-    //recibe y agrega un producto, y lo devuelve con su id asignado.
-    // http://localhost:8080/public/
-})
-
-//////2)
+///2) GET '/api/productos/:id' -> devuelve un producto según su id.
 
 
-// routerProductos.get("/api/productos/:id", (req, res, next)=>{ 
-//     res.json(productos);
+routerProductos.get("/:id", (req, res, next)=>{ 
 
-    //devuelve un producto según su id
-// })
+    contenedor.getById(req.params.id).then(data => {
+        if(data.id!=undefined){
+            res.json(data);
+        } else {
+            res.json({ error : 'producto no encontrado' });
+            
+        }
+    })
+});
 
-
-
-
-
-
-//////4)
-
-
-// routerProductos.put("/api/productos", (req, res, next)=>{ 
-//     res.json(productos);
-
-    //recibe y actualiza un producto según su id.
-// })
+//3) POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
 
 
-//////5)
+routerProductos.post('/', (req, res, next) => {
+    let producto = req.body;
+    contenedor.save(producto).then(data => {
+        res.json({atencion: "producto guardado"});
+    });
+});
+
+
+//4)PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
 
 
 
-// routerProductos.delete("/api/productos", (req, res, next)=>{ 
-//     res.json(productos);
+routerProductos.put("/:id", (req, res, next)=>{ 
+    let producto = req.body;
+    contenedor.update(req.params.id, producto).then(data => {
+        res.json(data)
+    });
+});
 
-//     //elimina un producto según su id.
-// })
+
+////5)DELETE '/api/productos/:id' -> elimina un producto según su id. */
+
+
+
+
+routerProductos.delete("/:id", (req, res, next)=>{ 
+    contenedor.deleteById(req.params.id).then(data => {
+        res.json({ atencion: 'producto eliminado' });
+    });
+});
 
 app.use("/api/productos", routerProductos);
 
 
-app.listen(PORT, ()=>{
+const server = app.listen(PORT, ()=>{
     console.log(`Helooo, estamos escuchando en esta uri: http://localhost:${PORT}`)
 });
 
-// routerMascotas.post("/", (req, res, next)=>{
-//     mascotas.push(req.body)
-//     res.json(mascotas);
-// })
-
-// routerPersonas.get("/", (req, res, next)=>{
-//     res.json(personas);
-// })
-
-// routerPersonas.post("/", (req, res, next)=>{
-//     personas.push(req.body)
-//     res.json(personas);
-// })
-
-
-// app.use("/mascotas", routerMascotas);
-
-// app.use("/personas", routerPersonas);
-
-///////////////////////////////////////AGREGAR UNA FOTO EN CARPETA UPLOADS///////////////
-
-// let storage = multer.diskStorage({
-//     destination: (req, file, cb) =>{
-//         cb(null, "uploads"); //callback error
-//     },
-//     filename: (req, file, cb)=>{
-//         cb(null, `${Date.now()}-${file.originalname} `);
-//     }
-// })
-
-// let ulploadMiddleare = multer({storage})
+//mensajes para errores del servidor
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message });
+});
+app.use((req, res, next) => {
+    res.status(404).json({ error: '404 - not found' });
+});
+app.use((err, req, res, next) => {
+    res.status(401).json({ error: '401 - not authorized' });
+});
+app.use((err, req, res, next) => {
+    res.status(403).json({ error: '403 - forbidden' });
+});
 
 
-// app.post("/uploadfile", ulploadMiddleare.single("myFile"), (req, res, next) => {
-//     let file = req.file;
-//     console.log("Entramos al post", file);
-//     if(!file){
-//         new Error("Error en la carga de la imagen");
-//     }
-//     res.send(file);
-// })
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-// app.post("/uploadmultifile", ulploadMiddleare.array("myFiles"), (req, res, next) => {
-//     let file = req.file;
-//     console.log("Entramos al post", file);
-//     if(!file){
-//         new Error("Error en la carga de la imagen");
-//     }
-//     res.send(file);
-// })
-
-
-
+server.on("error", error => console.log(error));
